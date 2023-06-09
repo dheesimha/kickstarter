e52583955f4dcd8b5a82607dd02b9743ec9fc716
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2023-06-09 14:58:38 +0530

    Fix build issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
-import fs from "fs"
-
-const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"))
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,10 +6,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: vercel({
-			external: Object.keys(packageJson.dependencies),
-			split: false
-		})
+		adapter: adapter()
 	}
 };
 
